bfe400b7bcf452ae78a80408127a1f9f1a613774
sushantshekhar-tech <shekharsushant54@gmail.com>
2024-11-29 21:05:33 +0530

    code cleanup

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/image-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/image-form.tsx
@@ -3,23 +3,10 @@
 import React, { useState } from "react";
 import * as z from "zod";
 import axios from "axios";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormMessage,
-} from "@/components/ui/form";
-
 import { Button } from "@/components/ui/button";
 import { ImageIcon, Pencil, PlusCircle } from "lucide-react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
-import { cn } from "@/lib/utils";
-import { Textarea } from "@/components/ui/textarea";
 import { Course } from "@prisma/client";
 import Image from "next/image";
 import { FileUpload } from "@/components/file-upload";
@@ -42,14 +29,7 @@ const ImageForm = ({ initialData, courseId }: ImageFormProps) => {
 
   const router = useRouter();
 
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      imageUrl: initialData?.imageUrl || "",
-    },
-  });
-
-  const { isSubmitting, isValid } = form.formState;
+  
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     // console.log(values);
