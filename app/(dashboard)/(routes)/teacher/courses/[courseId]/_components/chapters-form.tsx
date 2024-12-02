b35be4d9e54129b2689708bcdc9d11213d490987
sushantshekhar-tech <shekharsushant54@gmail.com>
2024-12-02 23:54:22 +0530

    Chapter reorder Functionility done

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -15,16 +15,17 @@ import {
 } from "@/components/ui/form";
 
 import { Button } from "@/components/ui/button";
-import { Pencil, PlusCircle } from "lucide-react";
+import { Loader2, Pencil, PlusCircle } from "lucide-react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
-import { Textarea } from "@/components/ui/textarea";
+
 import { Chapter, Course } from "@prisma/client";
 import { Input } from "@/components/ui/input";
+import ChaptersList from "./chapters-list";
 
 interface ChapterFormProps {
-  initialData: Course & {chapter : Chapter[]}
+  initialData: Course & {chapters : Chapter[]}
   courseId: string;
 }
 
@@ -61,8 +62,33 @@ const ChapterForm = ({ initialData, courseId }: ChapterFormProps) => {
     }
   };
 
+  const onReorder = async (updateData : {id : string;position: number}[])=>{
+    try{
+setIsUpdating(true);
+await axios.put(`/api/courses/${courseId}/reorder`,{
+  list:updateData
+});
+toast.success("Chapter reordered successfully")
+router.refresh();
+
+    }catch{
+      toast.error("Something went wrong")
+    }finally{
+      setIsUpdating(false)
+    }
+  };
+  
+  const onEdit =(id:string)=>{
+    router.push(`/teacher/courses/${courseId}/chapters/${id}`)
+  }
+
   return (
-    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+    <div className="relative mt-6 border bg-slate-100 rounded-md p-4">
+      {isUpdating && (
+        <div className="absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-m flex items-center justify-center ">
+          <Loader2 className="animate-spin h-6 w-6 text-sky-700"/>
+        </div>
+      )}
       <div className="font-medium flex item-center justify-between">
         Course Chapters
         <Button onClick={toggleCreating} variant="ghost">
@@ -109,13 +135,17 @@ const ChapterForm = ({ initialData, courseId }: ChapterFormProps) => {
        <div
        className={cn(
          "text-sm mt-2",
-         (!initialData.chapter || !initialData.chapter.length) && "text-slate-500 italics"
+         (!initialData.chapters || !initialData.chapters.length) && "text-slate-500 italics"
        )}
      >
-       {!initialData.chapter && "No Chapters"}
-       
-       {/* TODO : Add a list of chapters */}
-        {/* TODO : Fix the No chapter issue here  */}
+       {!initialData.chapters && "No Chapters"}
+       {/* TODO : Fix the No chapter issue here  */}
+
+       <ChaptersList
+       onEdit={onEdit}
+       onReorder = {onReorder}
+       items={initialData.chapters || []}
+       />
      </div>
      
       )}
---
+++ app/api/courses/[courseId]/reorder/route.ts
@@ -0,0 +1,44 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { courseId: string } }
+) {
+  try {
+    const { userId } = await auth();
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const { list } = await req.json();
+
+    const ownCourse = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId: userId,
+      },
+    });
+
+    if (!ownCourse) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    for (const item of list) {
+      await db.chapter.update({
+        where: {
+          id: item.id,
+        },
+        data: {
+          position: item.position,
+        },
+      });
+    }
+
+    return new NextResponse("Success", { status: 200 });
+  } catch (error) {
+    console.log("[REORDER]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
